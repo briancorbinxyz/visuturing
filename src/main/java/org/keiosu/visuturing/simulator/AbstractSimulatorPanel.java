11085d748e928278f504e0cae9d090b99724e99e
Brian Corbin <me@briancorbin.co.uk>
2020-07-04 15:35:10 -0400

    Partial variable cleanups

--- src/main/java/org/keiosu/visuturing/simulator/AbstractSimulatorPanel.java
+++ src/main/java/org/keiosu/visuturing/simulator/AbstractSimulatorPanel.java
@@ -5,10 +5,14 @@ import org.keiosu.visuturing.core.TuringMachine;
 import javax.swing.JPanel;
 
 public abstract class AbstractSimulatorPanel extends JPanel {
+
   private static final double MAX_SPEED = 12.0D;
   private static final double MIN_SPEED = 0.1D;
+  public static final double SPEED_DELTA = 0.5D;
+
   protected String inputWord;
   protected double speed;
+
   protected TuringMachine machine;
 
   public AbstractSimulatorPanel(TuringMachine machine) {
@@ -26,7 +30,7 @@ public abstract class AbstractSimulatorPanel extends JPanel {
   }
 
   public void decreaseSpeed() {
-    this.speed = this.speed > MIN_SPEED ? this.speed - 0.5D : MIN_SPEED;
+    this.speed = this.speed > MIN_SPEED ? this.speed - SPEED_DELTA : MIN_SPEED;
   }
 
   public void setInputWord(String inputWord) {
--- src/main/java/org/keiosu/visuturing/simulator/human/Tape.java
+++ src/main/java/org/keiosu/visuturing/simulator/human/Tape.java
@@ -10,14 +10,15 @@ import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Tape {
   public static final int NO_ANIMATION = -1;
   public static final Color TAPE_COLOUR = new Color(255, 255, 204);
   private Point2D location;
   private Font tapeFont;
-  private HashMap tapeContents;
+  private Map<Integer, String> tapeContents;
   private Rectangle cell;
   private int visibleCells;
   private int firstVisibleCell;
@@ -26,69 +27,64 @@ public class Tape {
     this.location = new Double(0.0D, 0.0D);
     this.moveTo(this.location);
     this.tapeFont = new Font("monospaced", 0, 17);
-    this.tapeContents = new HashMap();
+    this.tapeContents = new TreeMap<>();
     this.cell = new Rectangle(20, 22);
     this.visibleCells = 20;
     this.firstVisibleCell = -1;
   }
 
-  public Tape(Point2D var1) {
+  public Tape(Point2D location) {
     this();
-    this.moveTo(var1);
+    this.moveTo(location);
   }
 
-  public void moveTo(Point2D var1) {
-    this.location.setLocation(var1.getX(), var1.getY());
+  private void moveTo(Point2D location) {
+    this.location.setLocation(location.getX(), location.getY());
   }
 
-  public void moveTo(int var1, int var2) {
-    this.location.setLocation((double)var1, (double)var2);
-  }
-
-  public void draw(Graphics2D var1) {
-    var1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    var1.setFont(this.tapeFont);
-    double var2 = this.location.getX();
-    double var4 = this.location.getY();
-    String var6 = null;
+  public void draw(Graphics2D canvas) {
+    canvas.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    canvas.setFont(this.tapeFont);
+    double locationX = this.location.getX();
+    double locationY = this.location.getY();
+    String var6;
 
     double var7;
     double var9;
-    int var11;
-    for(var11 = this.firstVisibleCell; var11 < this.firstVisibleCell + this.visibleCells; ++var11) {
-      var1.setColor(TAPE_COLOUR);
-      var1.fill(new Rectangle((int)var2, (int)(var4 - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
-      var1.setColor(Color.BLACK);
-      var1.draw(new Rectangle((int)var2, (int)(var4 - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
-      var6 = (String)this.tapeContents.get(new Integer(var11));
-      var7 = var2 + (double)((this.getCellWidth() - var1.getFontMetrics().charWidth(Symbols.SPACE)) / 2);
-      var9 = var4 - (this.cell.getHeight() - (double)var1.getFontMetrics().getAscent()) / 2.0D;
-      if (var11 == -1) {
-        var1.drawString(String.valueOf(Symbols.LEFT_END_MARKER), (float)var7, (float)var9);
+    for(int currentCell = this.firstVisibleCell; currentCell < this.firstVisibleCell + this.visibleCells; ++currentCell) {
+      canvas.setColor(TAPE_COLOUR);
+      canvas.fill(new Rectangle((int)locationX, (int)(locationY - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
+      canvas.setColor(Color.BLACK);
+      canvas.draw(new Rectangle((int)locationX, (int)(locationY - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
+      var6 = (String)this.tapeContents.get(currentCell);
+      var7 = locationX + (double)((this.getCellWidth() - canvas.getFontMetrics().charWidth(Symbols.SPACE)) / 2);
+      var9 = locationY - (this.cell.getHeight() - (double) canvas.getFontMetrics().getAscent()) / 2.0D;
+      if (currentCell == -1) {
+        canvas.drawString(String.valueOf(Symbols.LEFT_END_MARKER), (float)var7, (float)var9);
       } else if (var6 == null) {
-        var1.drawString(String.valueOf(Symbols.SPACE), (float)var7, (float)var9);
+        canvas.drawString(String.valueOf(Symbols.SPACE), (float)var7, (float)var9);
       } else {
-        var1.drawString(var6, (float)var7, (float)var9);
+        canvas.drawString(var6, (float)var7, (float)var9);
       }
 
-      var2 += this.cell.getBounds().getWidth();
+      locationX += this.cell.getBounds().getWidth();
     }
 
-    for(var11 = 1; var11 < 6; ++var11) {
-      var7 = var2 + (double)((this.getCellWidth() - var1.getFontMetrics().charWidth(Symbols.SPACE)) / 2);
-      var9 = var4 - (this.cell.getHeight() - (double)var1.getFontMetrics().getAscent()) / 2.0D;
-      var1.setColor(new Color(TAPE_COLOUR.getRed(), TAPE_COLOUR.getGreen(), TAPE_COLOUR.getBlue(), 255 / var11));
-      var1.fill(new Rectangle((int)var2, (int)(var4 - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
-      var1.setColor(new Color(255 / (6 - var11), 255 / (6 - var11), 255 / (6 - var11)));
-      var1.draw(new Rectangle((int)var2, (int)(var4 - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
-      var1.drawString(String.valueOf(Symbols.SPACE), (float)var7, (float)var9);
-      var2 += this.cell.getBounds().getWidth();
+    for(int var11 = 1; var11 < 6; ++var11) {
+      var7 = locationX + (double)((this.getCellWidth() - canvas.getFontMetrics().charWidth(Symbols.SPACE)) / 2);
+      var9 = locationY - (this.cell.getHeight() - (double) canvas.getFontMetrics().getAscent()) / 2.0D;
+      canvas.setColor(new Color(TAPE_COLOUR.getRed(), TAPE_COLOUR.getGreen(), TAPE_COLOUR.getBlue(), 255 / var11));
+      canvas.fill(new Rectangle((int)locationX, (int)(locationY - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
+      canvas.setColor(new Color(255 / (6 - var11), 255 / (6 - var11), 255 / (6 - var11)));
+      canvas.draw(new Rectangle((int)locationX, (int)(locationY - this.cell.getBounds().getHeight()), (int)this.cell.getBounds().getWidth(), (int)this.cell.getBounds().getHeight()));
+      canvas.drawString(String.valueOf(Symbols.SPACE), (float)var7, (float)var9);
+      locationX += this.cell.getBounds().getWidth();
     }
 
-    var2 -= 5.0D * this.cell.getBounds().getWidth();
-    this.drawInfinite(var2, var4, var1);
+    locationX -= 5.0D * this.cell.getBounds().getWidth();
+    this.drawInfinite(locationX, locationY, canvas);
     if (this.firstVisibleCell > -1) {
-      this.drawInfinite(this.getPosition().getX(), this.getPosition().getY(), var1);
+      this.drawInfinite(this.getPosition().getX(), this.getPosition().getY(), canvas);
     }
 
   }
