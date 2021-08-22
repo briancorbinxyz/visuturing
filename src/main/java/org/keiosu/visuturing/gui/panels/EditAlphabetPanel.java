12aebce802cc6c2b827e628b5b2fe17b40b848f0
Brian Corbin <me@briancorbin.co.uk>
2021-08-21 22:13:56 -0400

    Fix EditAlphabetPanel

--- src/main/java/org/keiosu/visuturing/gui/panels/EditAlphabetPanel.java
+++ src/main/java/org/keiosu/visuturing/gui/panels/EditAlphabetPanel.java
@@ -15,170 +15,183 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import org.keiosu.visuturing.core.Symbols;
 import org.keiosu.visuturing.core.Transition;
 import org.keiosu.visuturing.core.TuringMachine;
 
 public class EditAlphabetPanel extends VTPanel implements ActionListener, ListSelectionListener {
+
+    public static final String ADD = "Add";
+    public static final String REMOVE = "Remove";
+    public static final String REMOVE_ALL = "Remove All";
+    public static final String ADD_0_1 = "Add {0,1}";
+    public static final String ADD_A_B = "Add {a,b}";
+    public static final String ADD_0_9 = "Add {0..9}";
+
     JTextField symbolField;
-    JList alphabetList;
+    JList<String> alphabetList;
     List<String> alphabet;
     JButton removeButton;
     JButton removeAllButton;
     TuringMachine machine;
 
-    public EditAlphabetPanel(TuringMachine var1) {
+    public EditAlphabetPanel(TuringMachine machine) {
         super("edit-alphabet-panel.gif");
-        this.machine = var1;
-        JLayeredPane var2 = new JLayeredPane();
+        this.machine = machine;
+        JLayeredPane pane = new JLayeredPane();
         this.panel.setLayout(new BorderLayout());
-        this.panel.add(var2);
-        var2.setPreferredSize(this.panel.getPreferredSize());
-        JLabel var3 = new JLabel("Symbol:");
-        var3.setBounds(0, 16, 65, 20);
+        this.panel.add(pane);
+        pane.setPreferredSize(this.panel.getPreferredSize());
+        JLabel symbolLabel = new JLabel("Symbol:");
+        symbolLabel.setBounds(0, 16, 65, 20);
         this.symbolField = new JTextField();
         this.symbolField.setBounds(79, 16, 119, 20);
-        JButton var4 = new JButton("Add");
-        var4.setName("Add");
-        var4.addActionListener(this);
-        var4.setBounds(205, 16, 95, 20);
-        JLabel var5 = new JLabel("Alphabet:");
-        var5.setBounds(0, 49, 65, 20);
-        this.alphabet = new ArrayList(var1.getAlphabet());
-        this.alphabetList = new JList(this.alphabet.toArray());
+        JButton addButton = new JButton(ADD);
+        addButton.setName(ADD);
+        addButton.addActionListener(this);
+        addButton.setBounds(205, 16, 95, 20);
+        JLabel alphabetLabel = new JLabel("Alphabet:");
+        alphabetLabel.setBounds(0, 49, 65, 20);
+        this.alphabet = new ArrayList<>(machine.getAlphabet());
+        this.alphabetList = new JList<>(this.alphabet.toArray(new String[] {}));
         this.alphabetList.addListSelectionListener(this);
-        this.alphabetList.setFont(new Font("sanserif", 0, 14));
-        JScrollPane var6 = new JScrollPane(this.alphabetList);
-        var6.setBounds(79, 48, 120, 170);
-        this.removeButton = new JButton("Remove");
-        this.removeButton.setName("Remove");
+        this.alphabetList.setFont(new Font("sanserif", Font.PLAIN, 14));
+        JScrollPane alphabetListPane = new JScrollPane(this.alphabetList);
+        alphabetListPane.setBounds(79, 48, 120, 170);
+        this.removeButton = new JButton(REMOVE);
+        this.removeButton.setName(REMOVE);
         this.removeButton.addActionListener(this);
         this.removeButton.setEnabled(false);
         this.removeButton.setBounds(205, 48, 95, 20);
-        this.removeAllButton = new JButton("Remove All");
-        this.removeAllButton.setName("Remove All");
+        this.removeAllButton = new JButton(REMOVE_ALL);
+        this.removeAllButton.setName(REMOVE_ALL);
         this.removeAllButton.addActionListener(this);
         this.removeAllButton.setEnabled(false);
         this.removeAllButton.setBounds(205, 76, 95, 20);
-        JButton var7 = new JButton("Add {0,1}");
-        var7.setName("Add {0,1}");
-        var7.addActionListener(this);
-        var7.setBounds(205, 142, 95, 20);
-        JButton var8 = new JButton("Add {a,b}");
-        var8.setName("Add {a,b}");
-        var8.addActionListener(this);
-        var8.setBounds(205, 170, 95, 20);
-        JButton var9 = new JButton("Add {0..9}");
-        var9.setName("Add {0..9}");
-        var9.addActionListener(this);
-        var9.setBounds(205, 198, 95, 20);
-        var2.add(var3, JLayeredPane.DEFAULT_LAYER);
-        var2.add(this.symbolField, JLayeredPane.DEFAULT_LAYER);
-        var2.add(var5, JLayeredPane.DEFAULT_LAYER);
-        var2.add(var4, JLayeredPane.DEFAULT_LAYER);
-        var2.add(var6, JLayeredPane.DEFAULT_LAYER);
-        var2.add(this.removeAllButton, JLayeredPane.DEFAULT_LAYER);
-        var2.add(this.removeButton, JLayeredPane.DEFAULT_LAYER);
-        var2.add(var7, JLayeredPane.DEFAULT_LAYER);
-        var2.add(var8, JLayeredPane.DEFAULT_LAYER);
-        var2.add(var9, JLayeredPane.DEFAULT_LAYER);
+        JButton binaryButton = new JButton(ADD_0_1);
+        binaryButton.setName(ADD_0_1);
+        binaryButton.addActionListener(this);
+        binaryButton.setBounds(205, 142, 95, 20);
+        JButton abButton = new JButton(ADD_A_B);
+        abButton.setName(ADD_A_B);
+        abButton.addActionListener(this);
+        abButton.setBounds(205, 170, 95, 20);
+        JButton numeralsButton = new JButton(ADD_0_9);
+        numeralsButton.setName(ADD_0_9);
+        numeralsButton.addActionListener(this);
+        numeralsButton.setBounds(205, 198, 95, 20);
+        pane.add(symbolLabel, JLayeredPane.DEFAULT_LAYER);
+        pane.add(this.symbolField, JLayeredPane.DEFAULT_LAYER);
+        pane.add(alphabetLabel, JLayeredPane.DEFAULT_LAYER);
+        pane.add(addButton, JLayeredPane.DEFAULT_LAYER);
+        pane.add(alphabetListPane, JLayeredPane.DEFAULT_LAYER);
+        pane.add(this.removeAllButton, JLayeredPane.DEFAULT_LAYER);
+        pane.add(this.removeButton, JLayeredPane.DEFAULT_LAYER);
+        pane.add(binaryButton, JLayeredPane.DEFAULT_LAYER);
+        pane.add(abButton, JLayeredPane.DEFAULT_LAYER);
+        pane.add(numeralsButton, JLayeredPane.DEFAULT_LAYER);
     }
 
-    public boolean hasTransitions(char var1) {
-        List var2 = this.machine.getTransitions();
-        boolean var3 = false;
-
-        for (int var4 = 0; var4 < var2.size(); ++var4) {
-            Transition var5 = (Transition) var2.get(var4);
-            if (var5.getCurrentSymbol() == var1 || var5.getTask() == var1) {
-                var3 = true;
-                return var3;
+    public boolean hasTransitions(char fromSymbol) {
+        List<Transition> transitions = this.machine.getTransitions();
+        for (Transition transition : transitions) {
+            if (transition.getCurrentSymbol() == fromSymbol || transition.getTask() == fromSymbol) {
+                return true;
             }
         }
 
-        return var3;
+        return false;
     }
 
-    public void actionPerformed(ActionEvent var1) {
-        if (var1.getSource() instanceof JButton) {
-            JButton var2 = (JButton) var1.getSource();
-            if (var2.getName().equals("Add")) {
-                String var3 = this.symbolField.getText();
-                if (var3 != null && var3.length() == 1) {
-                    if (!this.symbolExists(var3) && !this.isStandardSymbol(var3)) {
-                        this.alphabet.add(var3);
-                    }
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() instanceof JButton) {
+            JButton source = (JButton) event.getSource();
+            switch (source.getName()) {
+                case ADD:
+                    addActionPerformed();
+                    break;
+                case REMOVE:
+                    removeActionPerformed(source);
+                    break;
+                case REMOVE_ALL:
+                    removeAllActionPerformed(source);
+                    break;
+                case ADD_0_1:
+                    addSymbolsPerformed("0", "1");
+                    break;
+                case ADD_A_B:
+                    addSymbolsPerformed("a", "b");
+                    break;
+                case ADD_0_9:
+                    addSymbolsPerformed("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+                    break;
+            }
+        }
+    }
 
-                    this.alphabetList.setListData(this.alphabet.toArray());
-                    this.symbolField.setText("");
-                    if (!this.removeAllButton.isEnabled()) {
-                        this.removeAllButton.setEnabled(true);
-                    }
-                }
-            } else if (var2.getName().equals("Remove")) {
-                int var7 = this.alphabetList.getSelectedIndex();
-                if (var7 > -1) {
-                    if (!this.hasTransitions(((String) this.alphabet.get(var7)).charAt(0))) {
-                        this.alphabet.remove(var7);
-                        this.alphabetList.setListData(this.alphabet.toArray());
-                        var2.setEnabled(false);
-                    } else {
-                        JOptionPane.showMessageDialog(
-                                this,
-                                "The selected symbols is involved in one or more transitions.\n\nPlease delete these before removing the symbol.",
-                                "VisuTuring - Integrity Protect",
-                                0);
-                    }
-                }
-            } else if (var2.getName().equals("Remove All")) {
-                boolean var8 = false;
-                List var4 = new ArrayList();
+    private void addSymbolsPerformed(String... symbols) {
+        for (String symbol : symbols) {
+            this.addSymbol(symbol);
+        }
+        if (!this.removeAllButton.isEnabled()) {
+            this.removeAllButton.setEnabled(true);
+        }
+    }
 
-                for (int var5 = 0; var5 < this.alphabet.size(); ++var5) {
-                    char var6 = ((String) this.alphabet.get(var5)).charAt(0);
-                    if (this.isStandardSymbol("" + var6) || this.hasTransitions(var6)) {
-                        var4.add(new String("" + var6));
-                        if (!this.isStandardSymbol("" + var6)) {
-                            var8 = true;
-                        }
-                    }
-                }
+    private void removeAllActionPerformed(JButton button) {
+        boolean hasTransitions = false;
+        List<String> newAlphabet = new ArrayList<>();
 
-                this.alphabet = var4;
-                this.alphabetList.setListData(this.alphabet.toArray());
-                var2.setEnabled(false);
-                if (var8) {
-                    JOptionPane.showMessageDialog(
-                            this,
-                            "One or more of the selected symbols was involved\nin one or more transitions.\n\nPlease delete these before removing the symbol(s).",
-                            "VisuTuring - Integrity Protect",
-                            0);
+        for (String s : this.alphabet) {
+            char c = s.charAt(0);
+            if (this.isStandardSymbol("" + c) || this.hasTransitions(c)) {
+                newAlphabet.add("" + c);
+                if (!this.isStandardSymbol("" + c)) {
+                    hasTransitions = true;
                 }
-            } else if (var2.getName().equals("Add {0,1}")) {
-                this.addSymbol("0");
-                this.addSymbol("1");
-                if (!this.removeAllButton.isEnabled()) {
-                    this.removeAllButton.setEnabled(true);
-                }
-            } else if (var2.getName().equals("Add {a,b}")) {
-                this.addSymbol("a");
-                this.addSymbol("b");
-                if (!this.removeAllButton.isEnabled()) {
-                    this.removeAllButton.setEnabled(true);
-                }
-            } else if (var2.getName().equals("Add {0..9}")) {
-                this.addSymbol("0");
-                this.addSymbol("1");
-                this.addSymbol("2");
-                this.addSymbol("3");
-                this.addSymbol("4");
-                this.addSymbol("5");
-                this.addSymbol("6");
-                this.addSymbol("7");
-                this.addSymbol("8");
-                this.addSymbol("9");
-                if (!this.removeAllButton.isEnabled()) {
-                    this.removeAllButton.setEnabled(true);
-                }
+            }
+        }
+
+        this.alphabet = newAlphabet;
+        this.alphabetList.setListData(this.alphabet.toArray(new String[] {}));
+        button.setEnabled(false);
+        if (hasTransitions) {
+            JOptionPane.showMessageDialog(
+                    this,
+                    "One or more of the selected symbols are involved\nin one or more transitions.\n\nPlease delete these before removing the symbol(s).",
+                    "VisuTuring - Integrity Protect",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void removeActionPerformed(JButton button) {
+        int selectedIndex = this.alphabetList.getSelectedIndex();
+        if (selectedIndex > -1) {
+            if (!this.hasTransitions(this.alphabet.get(selectedIndex).charAt(0))) {
+                this.alphabet.remove(selectedIndex);
+                this.alphabetList.setListData(this.alphabet.toArray(new String[] {}));
+                button.setEnabled(false);
+            } else {
+                JOptionPane.showMessageDialog(
+                        this,
+                        "The selected symbol is involved in one or more transitions.\n\nPlease delete these before removing the symbol.",
+                        "VisuTuring - Integrity Protect",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void addActionPerformed() {
+        String symbolFieldText = this.symbolField.getText();
+        if (symbolFieldText != null && symbolFieldText.length() == 1) {
+            if (this.notInAlphabet(symbolFieldText) && !this.isStandardSymbol(symbolFieldText)) {
+                this.alphabet.add(symbolFieldText);
+            }
+
+            this.alphabetList.setListData(this.alphabet.toArray(new String[] {}));
+            this.symbolField.setText("");
+            if (!this.removeAllButton.isEnabled()) {
+                this.removeAllButton.setEnabled(true);
             }
         }
     }
@@ -187,45 +200,34 @@ public class EditAlphabetPanel extends VTPanel implements ActionListener, ListSe
         return this.alphabet;
     }
 
-    boolean symbolExists(String var1) {
-        boolean var2 = false;
-        int var3 = 0;
-
-        while (var3 < this.alphabet.size() && !var2) {
-            if (var1.equals((String) this.alphabet.get(var3++))) {
-                var2 = true;
+    boolean notInAlphabet(String symbol) {
+        boolean found = false;
+        int symbolIndex = 0;
+        while (symbolIndex < this.alphabet.size() && !found) {
+            if (symbol.equals(this.alphabet.get(symbolIndex++))) {
+                found = true;
             }
         }
-
-        return var2;
+        return !found;
     }
 
-    void addSymbol(String var1) {
-        if (!this.symbolExists(var1)) {
-            this.alphabet.add(var1);
+    void addSymbol(String symbol) {
+        if (notInAlphabet(symbol)) {
+            alphabet.add(symbol);
         }
-
-        this.alphabetList.setListData(this.alphabet.toArray());
+        this.alphabetList.setListData(this.alphabet.toArray(new String[] {}));
     }
 
-    boolean isStandardSymbol(String var1) {
-        boolean var2 = false;
-        if (var1.equals("⊳") || var1.equals("⊔")) {
-            var2 = true;
-        }
-
-        return var2;
+    boolean isStandardSymbol(String symbol) {
+        return symbol.equals(String.valueOf(Symbols.LEFT_END_MARKER))
+                || symbol.equals(String.valueOf(Symbols.SPACE));
     }
 
-    public void valueChanged(ListSelectionEvent var1) {
-        int var2 = this.alphabetList.getSelectedIndex();
-        if (var2 > -1) {
-            String var3 = (String) this.alphabet.get(var2);
-            if (this.isStandardSymbol(var3)) {
-                this.removeButton.setEnabled(false);
-            } else {
-                this.removeButton.setEnabled(true);
-            }
+    public void valueChanged(ListSelectionEvent event) {
+        int selectedIndex = this.alphabetList.getSelectedIndex();
+        if (selectedIndex > -1) {
+            String symbol = this.alphabet.get(selectedIndex);
+            this.removeButton.setEnabled(!this.isStandardSymbol(symbol));
         }
 
         if (this.alphabet.size() > 4) {
