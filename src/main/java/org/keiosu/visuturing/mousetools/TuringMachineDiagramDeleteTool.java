dbdc473764af656efba7f94bf6cb61964bdfeb06
Brian Corbin <me@briancorbin.co.uk>
2021-08-25 17:55:58 -0400

    Fix TuringMachineDiagramDeleteTool

--- src/main/java/org/keiosu/visuturing/mousetools/TuringMachineDiagramDeleteTool.java
+++ src/main/java/org/keiosu/visuturing/mousetools/TuringMachineDiagramDeleteTool.java
@@ -2,7 +2,6 @@ package org.keiosu.visuturing.mousetools;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Shape;
@@ -11,7 +10,6 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.CubicCurve2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.QuadCurve2D;
-import java.awt.geom.QuadCurve2D.Double;
 import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
@@ -32,92 +30,86 @@ public class TuringMachineDiagramDeleteTool extends TuringMachineDiagramTool {
                             this.currentTransition,
                             this.machine.stateFor(this.currentTransition.getCurrentState()));
             if (transitionCurve instanceof QuadCurve2D) {
-                QuadCurve2D var3 = (QuadCurve2D) transitionCurve;
-                Double var4 = new Double();
-                Double var5 = new Double();
-                var3.subdivide(var4, var5);
-                canvas.draw(
-                        new java.awt.geom.Line2D.Double(
-                                var4.getP2().getX() - 10.0D,
-                                var4.getP2().getY() - 10.0D,
-                                var4.getP2().getX() + 10.0D,
-                                var4.getP2().getY() + 10.0D));
-                canvas.draw(
-                        new java.awt.geom.Line2D.Double(
-                                var4.getP2().getX() - 10.0D,
-                                var4.getP2().getY() + 10.0D,
-                                var4.getP2().getX() + 10.0D,
-                                var4.getP2().getY() - 10.0D));
+                QuadCurve2D curve = (QuadCurve2D) transitionCurve;
+                drawCurveCross(canvas, curve);
             } else {
-                CubicCurve2D var7 = (CubicCurve2D) transitionCurve;
-                java.awt.geom.CubicCurve2D.Double var8 = new java.awt.geom.CubicCurve2D.Double();
-                java.awt.geom.CubicCurve2D.Double var6 = new java.awt.geom.CubicCurve2D.Double();
-                var7.subdivide(var8, var6);
-                canvas.draw(
-                        new java.awt.geom.Line2D.Double(
-                                var8.getP2().getX() - 10.0D,
-                                var8.getP2().getY() - 10.0D,
-                                var8.getP2().getX() + 10.0D,
-                                var8.getP2().getY() + 10.0D));
-                canvas.draw(
-                        new java.awt.geom.Line2D.Double(
-                                var8.getP2().getX() - 10.0D,
-                                var8.getP2().getY() + 10.0D,
-                                var8.getP2().getX() + 10.0D,
-                                var8.getP2().getY() - 10.0D));
+                CubicCurve2D curve = (CubicCurve2D) transitionCurve;
+                drawCurveCross(canvas, curve);
             }
         }
     }
 
-    public TuringMachineDiagramDeleteTool(DiagramEditor var1) {
-        super(var1);
+    private void drawCurveCross(Graphics2D canvas, QuadCurve2D curve) {
+        QuadCurve2D.Double lhs = new QuadCurve2D.Double();
+        curve.subdivide(lhs, null);
+        drawCurveCross(canvas, lhs.getP2());
+    }
+
+    private void drawCurveCross(Graphics2D canvas, CubicCurve2D curve) {
+        CubicCurve2D.Double lhs = new CubicCurve2D.Double();
+        curve.subdivide(lhs, null);
+        drawCurveCross(canvas, lhs.getP2());
+    }
+
+    private void drawCurveCross(Graphics2D canvas, Point2D p2) {
+        canvas.draw(
+                new java.awt.geom.Line2D.Double(
+                        p2.getX() - 10.0D,
+                        p2.getY() - 10.0D,
+                        p2.getX() + 10.0D,
+                        p2.getY() + 10.0D));
+        canvas.draw(
+                new java.awt.geom.Line2D.Double(
+                        p2.getX() - 10.0D,
+                        p2.getY() + 10.0D,
+                        p2.getX() + 10.0D,
+                        p2.getY() - 10.0D));
+    }
+
+    public TuringMachineDiagramDeleteTool(DiagramEditor editor) {
+        super(editor);
 
         try {
-            Toolkit var2 = Toolkit.getDefaultToolkit();
             this.setCursor(
-                    var2.createCustomCursor(
+                    Toolkit.getDefaultToolkit().createCustomCursor(
                             this.createImageIcon("cursors/delete.gif").getImage(),
                             new Point(9, 9),
                             "Remove"));
             this.setOverCursor(this.cursor);
-        } catch (Exception var3) {
+        } catch (Exception ignored) {
+            // do nothing
         }
     }
 
-    public void mousePressed(MouseEvent var1) {
-        if (SwingUtilities.isRightMouseButton(var1)) {
+    public void mousePressed(MouseEvent event) {
+        if (SwingUtilities.isRightMouseButton(event)) {
             this.diagramEditor.revertToSelect();
             this.diagramEditor.repaint();
         }
 
         if (this.currentTransition != null) {
-            JOptionPane var2 = new JOptionPane();
-            int var3 =
-                    JOptionPane.showConfirmDialog(
-                            (Component) null,
-                            "Are you sure you want to remove the transition: "
-                                    + this.currentTransition.toString()
-                                    + " ?",
-                            "VisuTuring - Transition Removal",
-                            1);
-            if (var3 == 0) {
+            if (JOptionPane.showConfirmDialog(
+                null,
+                    "Are you sure you want to remove the transition: "
+                            + this.currentTransition
+                            + " ?",
+                    "VisuTuring - Transition Removal",
+                JOptionPane.YES_NO_CANCEL_OPTION) == 0) {
                 this.diagramEditor.getCurrentMachine().removeTransition(this.currentTransition);
                 this.diagramEditor.repaint();
             }
         }
     }
 
-    public void mouseMoved(MouseEvent var1) {
-        Point var2 = var1.getPoint();
-        Point2D var3 =
+    public void mouseMoved(MouseEvent event) {
+        Point eventPoint = event.getPoint();
+        Point2D userPoint =
                 this.diagramEditor.toUser(
-                        new java.awt.geom.Point2D.Double(var2.getX(), var2.getY()));
-        List var4 = this.diagramEditor.getCurrentMachine().getTransitions();
-
-        for (int var5 = 0; var5 < var4.size(); ++var5) {
-            Transition var6 = (Transition) var4.get(var5);
-            if (this.isOver(var6, var3)) {
-                this.currentTransition = var6;
+                        new java.awt.geom.Point2D.Double(eventPoint.getX(), eventPoint.getY()));
+        List<Transition> transitions = this.diagramEditor.getCurrentMachine().getTransitions();
+        for (Transition transition : transitions) {
+            if (this.isOver(transition, userPoint)) {
+                this.currentTransition = transition;
                 this.diagramEditor.repaint();
                 return;
             }
